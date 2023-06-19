c326d5d95cadfe4fbca7f874784d72d6048c0db1
Bishop <christianiradukunda6@gmail.com>
2023-06-19 13:25:14 +0200

    added editform and form validation

--- BlazzorOverview/Data/Grocery.cs
+++ BlazzorOverview/Data/Grocery.cs
@@ -1,9 +1,14 @@
-﻿namespace BlazzorOverview.Data
+﻿using System.ComponentModel.DataAnnotations;
+namespace BlazzorOverview.Data
 {
     public class Grocery
     {
+        [Required]
+        [StringLength(15,ErrorMessage="Name should be less than 15 characters")]
         public string Name { get; set; }
 
+        [Required]
+        [Range(1,10000,ErrorMessage ="The range should be between 1-1000")]
         public float Price { get; set; }
     }
 }
